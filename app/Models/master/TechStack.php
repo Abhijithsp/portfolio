24f940d312c521d664461c710ff7f34791c0d7ca
Abhijith <abhijithsp1@gmail.com>
2024-11-17 22:13:48 +0530

    reade me changes

---
+++ app/Models/master/TechStack.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\master;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TechStack extends Model
+{
+    //
+}
---
+++ database/migrations/2024_11_16_184922_create_user_infos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->nullable();
+            $table->string('name')->nullable();
+            $table->string('user_photo')->nullable();
+            $table->string('title')->nullable();
+            $table->string('github_url')->nullable();
+            $table->string('linked_in_url')->nullable();
+            $table->string('instagram_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_infos');
+    }
+};
